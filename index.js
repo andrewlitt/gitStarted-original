7c52dbfd386606a7513a26e6d94b6c3c9c7a5327
Jalsemgeest <alsemgeester@gmail.com>
2016-03-05 13:03:51 -0500

    Setup the GitHub connection.  We store the GitHub information in a session variable and need to re-create the connection if we're in a new route. This way we don't have to worry about closing them or anything.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var stylus = require('stylus');
 var github = require('octonode');
+var Handlebars = require('handlebars');
 
 // Controllers
 var models = require('./models/modelHelper.js');
@@ -37,6 +38,10 @@ var sess = session({
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
  
+app.use(function(req, res, next){
+    res.locals.session = req.session;
+    next();
+});
 
 
 // Views
@@ -51,10 +56,23 @@ app.get('/content', sess, content);
 
 // Getting Post information
 app.post('/content', function(req, res) {
-	console.log(req.body.email);
-	req.session.userId = req.body.email;
-    console.log(req.session.userId);
-    res.redirect('/content');
+    if (req.body.username == 'undefined' || req.body.password == 'undefined') {
+    	res.redirect('/');
+    } else {
+    	var username = req.body.username, password = req.body.password;
+    	var client = github.client({
+				  username: username,
+				  password: password
+				});
+    	req.session.client = client;
+    	client.get('/user', {}, function (err, status, body, headers) {
+			  req.session.userInfo = body; //json object
+			  client.get('/user/following', {}, function (err, status, body, headers) {
+		    		req.session.following = body;
+		    		res.redirect('/content');
+		    	});
+			});
+    }
 });
 
 var server = app.listen(3000, function () {
--- routes/content.js
+++ routes/content.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var path = require('path');
+var github = require('octonode');
+var Handlebars = require('handlebars');
+var fs = require('fs');
 
 module.exports = (function() {
     'use strict';
     var router = express.Router();
 
     router.get('/content', function(req, res) {
-    	console.log(req.session);
-        res.sendFile(path.join(__dirname + '/../views/content.html'));
+    	// console.log(req.session);
+    	// console.log(req.session.following);
+    	var username = req.session.client.token.username;
+    	var password = req.session.client.token.password;
+    	var client = github.client({
+				  username: username,
+				  password: password
+				});
+
+    	client.get('/user/repos', {}, function (err, status, body, headers) {
+    		fs.readFile(path.join(__dirname + '/../views/content.html'), 'utf-8', function(err, data) {
+    			var template = Handlebars.compile(data);
+    			res.send(template({"username":username}));
+    		});
+    	});
     });
 
     return router;    
