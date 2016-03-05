62b9d38714b2b9add92bcfd47e9038d64ff18000
zharley18 <zackharley@gmail.com>
2016-03-05 12:22:48 -0500

    Change file structure. Add json version of file structure and files.

--- controllers/test.js
+++ controllers/test.js
@@ -3,10 +3,7 @@ var http = require('http');
 var port = 3000;
 
 var server = http.createServer(function(req, res) {
-    var requireStatement = helper.getRequireStatement('http');
-    var fileIO = helper.populateDirectory('zack');
-    console.log('fileIO: ' + fileIO);
-    helper.appendFile('users/zack/index.js', requireStatement);
+    helper.populateDirectory()
     
     //console.log(helper.getRequireStatement('http'));
 });
